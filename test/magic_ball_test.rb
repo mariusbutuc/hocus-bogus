d3f2002ac042caa300e6a329db7c53e717960335
Marius Butuc <marius.butuc@gmail.com>
2016-10-21 17:14:54 -0400

    Keep dependencies local

--- test/magic_ball_test.rb
+++ test/magic_ball_test.rb
@@ -1,4 +1,5 @@
 require_relative 'test_helper'
+require_relative '../lib/magic_ball'
 
 class MagicBallTest < Minitest::Test
   def test_ask_returns_an_answer
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,4 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
-require_relative '../lib/magic_ball'
 
 Minitest::Reporters.use!
